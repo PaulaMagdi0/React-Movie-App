e8ca77f11f2cbe3506fe3ca2ad4344cf0ee3c6fe
Paula Magdy <paulamagdy665@gmail.com>
2025-01-06 14:02:47 +0200

    API

--- env.js
+++ env.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_BASE_URL='https://api.themoviedb.org/3/movie/popular?api_key=a8076e672805353996a77d9c2e8db7ee';
+const API_BASE_URL='https://api.themoviedb.org/3/movie/popular?api_key={API}';
 import.meta.env.VITE_API_BASE_URL;
 export const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
